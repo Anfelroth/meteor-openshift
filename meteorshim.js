ffce9fd06b209e57f0fa6b04f7e3862ec063bf9a
toni <toni@flipship.net>
2016-06-12 01:59:27 +0200

    typo

--- meteorshim.js
+++ meteorshim.js
@@ -4,7 +4,7 @@
 var APP_DNS = process.env.OPENSHIFT_APP_DNS_ALIAS || process.env.OPENSHIFT_APP_DNS;
 
 process.env.ROOT_URL = "https://" + APP_DNS || "http://localhost";
-process.env.MONGO_URL = process.env.MOGOLAB_URI || process.env.MONGODB_URL;
+process.env.MONGO_URL = process.env.MONGOLAB_URI || process.env.MONGODB_URL;
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 process.env.DDP_DEFAULT_CONNECTION_URL = 'http://' + APP_DNS + ':8443';
